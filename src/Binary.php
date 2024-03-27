6c8e4c43cad9c44e1f967fb82af09d7fa816b5bf
Timo Poppinga <timo.poppinga@zdrei.com>
2024-03-27 09:29:20 +0100

    [TASK] #1 Add new DataType Binary /s1

---
+++ src/Binary.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Struct\DataType;
+
+use function bin2hex;
+use function hex2bin;
+
+final class Binary extends AbstractDataType
+{
+    protected string $binaryString = '';
+
+    protected function _deserializeFromString(string $serializedData): void
+    {
+        $deserializedData = hex2bin($serializedData);
+        if ($deserializedData === false) {
+            throw new \InvalidArgumentException('The $serializedData must be an valid hex string', 1711527719);
+        }
+        $this->binaryString = $deserializedData;
+    }
+
+    protected function _serializeToString(): string
+    {
+        $serializedData = bin2hex($this->binaryString);
+        return $serializedData;
+    }
+
+    public function getBinaryString(): string
+    {
+        return $this->binaryString;
+    }
+
+    public function setBinaryString(string $binaryString): void
+    {
+        $this->binaryString = $binaryString;
+    }
+}
